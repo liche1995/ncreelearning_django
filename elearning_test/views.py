b5f5763d9b9d60414a5b9bca328ab49e5a2170cd
闕立奇 <liche0241@gmail.com>
2021-01-21 17:12:44 +0800

    initial web

---
+++ elearning_test/views.py
@@ -0,0 +1,63 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from django.views import View
+from django import template
+import os
+import time
+
+
+def welcome(request):
+    return HttpResponse("hello world!")
+
+
+class WebCore(View):
+    work_dir = os.path.abspath(os.path.dirname(os.getcwd())) + '\\elearning_test\\'
+
+    def get(self, request):
+        context = {'time': self.now_time}
+        return render(request, self.work_dir+'templates\\index_.html', context)
+
+    @staticmethod
+    def now_time():
+        t = time.localtime()
+        result = time.strftime("%Y/%m/%d, %H:%M:%S", t)
+        return result
+
+
+class Clockview(View):
+    work_dir = os.path.abspath(os.path.dirname(os.getcwd())) + '\\elearning_test\\'
+
+    def get(self, request):
+        w = float(request.GET['utc'])
+        static_web = self.work_dir + "templates\\clock.html"
+        context = {'utc': w, 'hour': int(w), 'min': 0, 'timezone': False}
+        return render(request, static_web, context)
+
+
+class Attri(View):
+    work_dir = os.path.abspath(os.path.dirname(os.getcwd())) + '\\elearning_test\\'
+
+    def get(self, request):
+        getd = request.GET
+        static_web = self.work_dir + "templates\\test.html"
+        di = {"name": "闕立奇", "age": 25}
+        return render(request, static_web, locals())
+
+
+# 啟動首頁
+def callindex(request):
+    context = {}
+    return render(request, "common/index.html", context)
+
+
+# 課程頁
+def callesson(request):
+    context = {}
+    return render(request, "common/lesson.html", context)
+
+
+# 課程頁
+def callogin(request):
+    context = {}
+    return render(request, "account/login.html", context)
