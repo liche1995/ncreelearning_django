9e7ced64546e60cbc8ee28b0123b29a9fd9f32c5
闕立奇 <liche0241@gmail.com>
2022-02-11 17:06:36 +0800

    rollcall link lessoninfo

--- elearning_test/rollcall_app/views.py
+++ elearning_test/rollcall_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from lesson_app import models as lesson_models
+from . import models as rollcall_models
 
 
 # Create your views here.
-def info(request):
+@login_required
+def view_init(request):
     context = {}
+    lesson_info = lesson_models.Lesson.objects.filter(auth=request.user.id)
+    context["lesson_info"] = lesson_info
     return render(request, "rollcall/info.html", context)
+
+
+def lesson_sub_info(request):
+    context = {}
+    context["subject_lesson"] = lesson_models.LessonTable.objects.filter(lesson_id_id=int(request.GET.get("lessonid","")))
+    return render(request, "rollcall/sub_info.html", context)
