d80f034dfe9120070d81721bec2b8e088c60a092
闕立奇 <liche0241@gmail.com>
2022-02-22 17:16:06 +0800

    fix rollcall table join error

--- elearning_test/rollcall_app/views.py
+++ elearning_test/rollcall_app/views.py
@@ -31,10 +31,12 @@ def call_table(request):
 
     subject_object = lesson_models.LessonTable.objects.get(inner_id=int(subject_id))
 
-    order = "select * from  rollcall as r " \
-            "right outer join lesson_studentlist as s " \
-            "on r.user_id = s.student_id " \
-            "where lessontableid_id = " + subject_id + " or lesson_id_id = " + str(subject_object.lesson_id.lessonid)
+    order = "select * from lesson_studentlist as s " \
+            "left outer join lesson_table as lt " \
+            "on s.lesson_id_id = lt.lesson_id_id " \
+            "left outer join rollcall as r " \
+            "on r.user_id = s.student_id and r.lessontableid_id = lt.inner_id " \
+            "where lt.inner_id = " + subject_id
 
     context["rollcall"] = rollcall_models.Rollcall.objects.raw(order)
     context["subject_id"] = subject_id
@@ -43,28 +45,35 @@ def call_table(request):
 
 def submit_rollcall(request):
     context = {}
+    context["msg"] = "發生錯誤"
     # 抓取資料
     if request.method.lower() != 'post':
-        context["msg"] = False
+        context["msg"] = "發生錯誤"
     else:
         rollcall_time = request.POST.getlist("manual_rollcall_time", [])
         student_list = request.POST.getlist("student_id", [])
         subject_id = int(request.POST.get("subject_id", ""))
+        enable_manual = request.POST.getlist("manual_enable", [])
 
         # 合併資料
 
         rollcall_table = [
-            [int(student_list[i]), int(rollcall_time[i] if rollcall_time[i] != "" else 0)]
+            [int(student_list[i]), float(rollcall_time[i] if rollcall_time[i] != "" else 0), enable_manual[i]]
             for i in range(len(student_list))]
 
-        # 調閱、新增或更新
+        # 新增或更新
         for student in rollcall_table:
-            try:
-                info = rollcall_models.Rollcall.objects.get(user_id=student[0])
-            except rollcall_models.Rollcall.DoesNotExist:
-                rollcall_models.Rollcall.objects.create(
-                    lessontableid_id=subject_id, user_id=student[0], sign_in_time=datetime.datetime.fromtimestamp(student[1]))
+            if student[3]:
+                try:
+                    info = rollcall_models.Rollcall.objects.get(user_id=student[0])
+                    info.sign_in_time = datetime.datetime.fromtimestamp(student[1])
+                    info.save()
+                except rollcall_models.Rollcall.DoesNotExist:
+                    rollcall_models.Rollcall.objects.create(
+                        lessontableid_id=subject_id, user_id=student[0], sign_in_time=datetime.datetime.fromtimestamp(student[1]))
+            else:
+                continue
 
-        context["msg"] = True
+        context["msg"] = "完成上傳"
 
     return JsonResponse(context)
